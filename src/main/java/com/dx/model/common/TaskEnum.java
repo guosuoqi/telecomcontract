00a392b104720e6581cf35a1c08eecad4b6e2896
guosuoqi <shukcyb@163.com>
2019-07-18 18:39:37 +0800

    合同管理页面

---
+++ src/main/java/com/dx/model/common/TaskEnum.java
@@ -0,0 +1,44 @@
+package com.dx.model.common;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public enum TaskEnum {
+
+	SEND_EMAIL(1,"发送邮件通知");
+
+    private Integer key;
+    
+    private String desc;
+    
+    TaskEnum(Integer key, String desc){
+        this.key=key;this.desc=desc;
+    }
+
+    public static TaskEnum getEnumByKey(Integer key){
+        if(key!=null){
+            for(TaskEnum item:TaskEnum.values()){
+                if(item.getKey().equals(key)){
+                    return item;
+                }
+            }
+        }
+        return null;
+    }
+    
+    public Integer getKey() {
+        return key;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+    
+	public static Map<Object,Object> getEnumMap(){
+		Map<Object,Object> map = new HashMap<Object,Object>();
+		for(TaskEnum item : TaskEnum.values()){
+			map.put(item.getKey(),item.getDesc());
+		}
+		return map;
+	} 
+}
