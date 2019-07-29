9ae96269eeef3253241202a105a3bf22ff194db6
guosuoqi <shukcyb@163.com>
2019-07-29 16:14:06 +0800

    时间类 -无BUG终版

--- src/main/java/com/dx/util/disposeUtil.java
+++ src/main/java/com/dx/util/disposeUtil.java
@@ -10,12 +10,12 @@ import java.util.GregorianCalendar;
 public class disposeUtil {
 
     public static void main(String[] args) {
-        Double dd=12.0;
-        System.out.println(getDateTime("2019-07-29 14:42:00",dd));
+        Double dd=10.0;
+        System.out.println(getDateTime("2019-09-29 14:42:00",dd,0));
     }
 
     //主流程
-    public static String getDateTime(String beginTime, Double num){
+    public static String getDateTime(String beginTime, Double num,Integer cu){
          Integer WORKING_TIME=7;
         if(beginTime==null || num==null){
             System.out.println("参数不正确");
@@ -23,7 +23,7 @@ public class disposeUtil {
         }
         try {
             SimpleDateFormat sDateFormat=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-            Date date=isWorkTime(beginTime,1);
+            Date date=isWorkTime(beginTime,1,cu);
             /*date=sDateFormat.parse(beginTime);*/
             System.out.println(sDateFormat.format(date));
 
@@ -39,13 +39,15 @@ public class disposeUtil {
                     date=getNextDate(date,1,null);
                     days++;
                 }
-                if(ceil!=0){
+                if(ceil>0){
                     days++;
                 }
             }
             date = getNextDate(sDateFormat.parse(beginTime), days, minutes);
             String format = sDateFormat.format(date);
-            date=isWorkTime(format,2);
+            if(cu==0){
+                return getDateTime(format, 0.0, 1);
+            }
             return sDateFormat.format(date);
         } catch (ParseException e) {
             System.out.println("转换异常");
@@ -53,7 +55,7 @@ public class disposeUtil {
         }
     }
 
-    private static Date isWorkTime(String date,Integer type) {
+    private static Date isWorkTime(String date,Integer type,Integer cu) {
         DateFormat df = new SimpleDateFormat("HH:mm");//创建日期转换对象HH:mm:ss为时分秒，年月日为yyyy-MM-dd
         SimpleDateFormat sDateFormat=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         DateFormat ymd=new SimpleDateFormat("yyyy-MM-dd");
@@ -79,8 +81,8 @@ public class disposeUtil {
                     ymdParam = getNextDate(ymdParam, 1, 0);
                     model=jointDateTime(ymdParam.getTime()+amStart.getTime(),model);
                 }
-            }else {
-                if(param.getTime() >= amEnd.getTime() && param.getTime() <=pmEnd.getTime()){
+            }else if(type==2 && cu==0){
+                if(param.getTime() >= amEnd.getTime() && param.getTime() <=amEnd.getTime()){
                     calendar.setTime(model);
                     calendar.add(Calendar.MINUTE,minute);
                     model = calendar.getTime();
@@ -91,7 +93,7 @@ public class disposeUtil {
                     minute= Math.toIntExact(time / 1000 / 60);
                     calendar.setTime(ymdParam);
                     calendar.add(Calendar.MINUTE,minute);
-                    String dateTime = getDateTime(sDateFormat.format(calendar.getTime()), 0.0);
+                    String dateTime = getDateTime(sDateFormat.format(calendar.getTime()), 0.0,cu++);
                     model=sDateFormat.parse(dateTime);
                 }
             }
@@ -120,41 +122,20 @@ public class disposeUtil {
         }
         return false;
     }
-    public void isOverDay(Date d) { // 指定时间判断是否是双休日
-        DateFormat df = new SimpleDateFormat("yy-MM-dd");
-        try {
-            d = df.parse("");
-            if (d.getDay() == 0 || d.getDay() == 6) {
-                System.out.println("日期:[" + df.format(d) + "] 是双休日");
-            } else {
-                System.out.println("日期:[" + df.format(d) + "] 不是双休日");
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
 
-
-    public static boolean isHodliDays(Date date) { // 判断是否滴节假日，是否有节假日加班
+    public static boolean isHodliDays(Date date) {
         // 中国法定节假日期
-        String isHoliday = "01-01,01-02,01-03,02-09,02-10,02-11,02-12,02-13,02-14,"
-                + "02-15,04-04,04-05,04-06,04-29,04-30,05-01,06-10,06-11,"
-                + "06-12,09-19,09-20,09-21,10-01,10-02,10-03,10-04,10-05,10-06,10-07";
-
-
-
+        String isHoliday = "05-01,05-02,05-03,06-07,09-13,10-01,10-02,10-03,10-04,10-05";
         DateFormat df = new SimpleDateFormat("MM-dd");
         String d = df.format(date);
-
         if (isHoliday.contains(d)) {
-            System.out.println("是节假日...");
             return true;
         }
         return false;
     }
     public static boolean isOverDays(Date date) { // 判断是否滴节假日，是否有节假日加班
         // 节假前后加班日期
-        String overDay = "01-05,01-06,02-16,02-17,04-07,04-17,04-28,06-08,06-09,09-22,09-19,10-12";
+        String overDay = "05-05,09-29,10-12";
         DateFormat df = new SimpleDateFormat("MM-dd");
         String d = df.format(date);
         if (overDay.contains(d)) {
