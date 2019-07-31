ad26cdcc25d2546879af3fc856bd6e2e7ed14e8e
[17319260809] <17319260809@163.com>
2019-07-31 15:43:55 +0800

    角色权限管理

---
+++ src/main/java/com/dx/model/nav/NavMenuBean.java
@@ -0,0 +1,46 @@
+package com.dx.model.nav;
+
+
+public class NavMenuBean {
+
+	private Integer id;
+	
+	private String name;
+	
+	private String url;
+	
+	private Integer navId;
+
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public Integer getNavId() {
+		return navId;
+	}
+
+	public void setNavId(Integer navId) {
+		this.navId = navId;
+	}
+}
